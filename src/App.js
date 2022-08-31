f140da4e146cbda6f2476c1c26fe5cf24ffcaffe
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-08-31 22:27:12 +0530

    react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div>
+      Angad Kumar Mandal
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
